54b439418e568d0c67cb1d3e18cba3b15d8333ab
jamazzo <jmazzola21@amherst.edu>
2020-11-10 20:07:50 -0500

    updates

--- scraping-Jamie.R
+++ scraping-Jamie.R
@@ -15,10 +15,16 @@ hov_tables <- hov_url %>%
   read_html() %>%
   html_nodes("table")
 
-hov_studio <- html_table(hov_tables[[2]], fill = TRUE)
+hov_studio <- html_table(hov_tables[[2]], fill = TRUE) 
+hov_studio <- hov_studio %>%
+  filter(`Title` != "Title") %>%
+  select(`Title`, `Album details`, `Sales`)
 
 bey_tables <- bey_url %>%
   read_html() %>%
   html_nodes("table")
 
 bey_studio <- html_table(bey_tables[[2]], fill = TRUE)
+bey_studio <- bey_studio %>%
+  filter(`Title` != "Title") %>%
+  select(`Title`, `Album details`, `Sales`)
